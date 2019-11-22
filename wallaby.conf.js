42e41c6d60ef67c233eeb4e47d9ad6833f6ecddd
DarrenPaulWright <darren@darrenpaulwright.com>
2019-11-22 14:15:23 -0800

    Using karma-webpack-bundle

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,34 +1,6 @@
-const wallabyWebpack = require('wallaby-webpack');
-const testRunnerConfig = require('test-runner-config');
-const config = require('./testRunner.config.js');
+const wallabyConfig = require('karma-webpack-bundle').wallabyConfig;
+const testRunnerConfig = require('./testRunner.config.js');
 
-const files = testRunnerConfig.getWallabyFiles(config, {
-	css: (file) => {
-		return {pattern: file, instrument: false, load: true};
-	},
-	helper: (file) => {
-		return {pattern: file, instrument: false, load: false};
-	},
-	src: (file) => {
-		return {pattern: file, instrument: true, load: false};
-	},
-	specs: (file) => {
-		return {pattern: file, instrument: false, load: false};
-	}
+module.exports = wallabyConfig(testRunnerConfig, {
+	name: 'type-enforcer-ui'
 });
-
-module.exports = function(wallaby) {
-	return {
-		name: 'type-enforcer-ui',
-		files: files.files,
-		tests: files.tests,
-		testFramework: 'mocha',
-		env: {kind: 'chrome'},
-		postprocessor: wallabyWebpack(),
-		compilers: {'**/*.js': wallaby.compilers.babel()},
-		setup() {
-			window.__moduleBundler.loadTests();
-		},
-		lowCoverageThreshold: 100
-	};
-};
