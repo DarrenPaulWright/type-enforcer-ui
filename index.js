300642b8832c7be2d35a56276ae7f793d9fb3b1f
DarrenPaulWright <Darren@DarrenPaulWright.com>
2019-12-04 11:17:39 -0800

    there's no longer anything else to export from CssSize.js

--- index.js
+++ index.js
@@ -48,7 +48,6 @@ export { default as methodDockPoint } from './src/methods/methodDockPoint';
 export { default as methodElement } from './src/methods/methodElement';
 export { default as methodThickness } from './src/methods/methodThickness';
 
-export * from './src/CssSize';
 export * from './src/utility/cssUnits';
 export { default as CssSize } from './src/CssSize';
 export { default as DockPoint } from './src/DockPoint';
