004f7a249c9ccba8067d4e80abe05b6fd16b315b
DarrenPaulWright <darren@darrenpaulwright.com>
2020-08-24 08:31:26 -0500

    Adding file extensions to exports

--- index.js
+++ index.js
@@ -14,26 +14,26 @@
  */
 export * from 'type-enforcer-math';
 
-export { default as is } from './src/checks/is';
-export { default as isCssSize } from './src/checks/isCssSize';
-export { default as isDockPoint } from './src/checks/isDockPoint';
-export { default as isElement } from './src/checks/isElement';
-export { default as isThickness } from './src/checks/isThickness';
+export { default as is } from './src/checks/is.js';
+export { default as isCssSize } from './src/checks/isCssSize.js';
+export { default as isDockPoint } from './src/checks/isDockPoint.js';
+export { default as isElement } from './src/checks/isElement.js';
+export { default as isThickness } from './src/checks/isThickness.js';
 
-export { default as enforce } from './src/enforcer/enforce';
-export { default as enforceCssSize } from './src/enforcer/enforceCssSize';
-export { default as enforceDockPoint } from './src/enforcer/enforceDockPoint';
-export { default as enforceElement } from './src/enforcer/enforceElement';
-export { default as enforceThickness } from './src/enforcer/enforceThickness';
+export { default as enforce } from './src/enforcer/enforce.js';
+export { default as enforceCssSize } from './src/enforcer/enforceCssSize.js';
+export { default as enforceDockPoint } from './src/enforcer/enforceDockPoint.js';
+export { default as enforceElement } from './src/enforcer/enforceElement.js';
+export { default as enforceThickness } from './src/enforcer/enforceThickness.js';
 
-export { default as method } from './src/methods/method';
-export { default as methodCssSize } from './src/methods/methodCssSize';
-export { default as methodDockPoint } from './src/methods/methodDockPoint';
-export { default as methodElement } from './src/methods/methodElement';
-export { default as methodThickness } from './src/methods/methodThickness';
+export { default as method } from './src/methods/method.js';
+export { default as methodCssSize } from './src/methods/methodCssSize.js';
+export { default as methodDockPoint } from './src/methods/methodDockPoint.js';
+export { default as methodElement } from './src/methods/methodElement.js';
+export { default as methodThickness } from './src/methods/methodThickness.js';
 
 export * from './src/utility/cssUnits';
-export { default as CssSize } from './src/CssSize';
-export { default as DockPoint } from './src/DockPoint';
-export { default as Thickness } from './src/Thickness';
-export { default as windowResize } from './src/windowResize';
+export { default as CssSize } from './src/CssSize.js';
+export { default as DockPoint } from './src/DockPoint.js';
+export { default as Thickness } from './src/Thickness.js';
+export { default as windowResize } from './src/windowResize.js';
